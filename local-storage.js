c53a75b3269264785ad82a46f076e3e4aefe35d3
Christian Bundy <christianbundy@fraction.io>
2020-01-22 09:06:24 -0800

    Experiment with common-good

--- local-storage.js
+++ local-storage.js
@@ -1,37 +1,31 @@
-'use strict'
-var u = require('./util')
+"use strict";
+var u = require("./util");
 
-function isFunction (f) {
-  return 'function' == typeof f
+function isFunction(f) {
+  return "function" == typeof f;
 }
 
-module.exports = function (generate) {
-
-  function create (filename, curve, legacy) {
-    var keys = generate(curve, legacy)
-    localStorage[filename] = JSON.stringify(keys)
-    return keys
+module.exports = function(generate) {
+  function create(filename, curve, legacy) {
+    var keys = generate(curve, legacy);
+    localStorage[filename] = JSON.stringify(keys);
+    return keys;
   }
 
-  function load (filename) {
-    return JSON.parse(localStorage[filename])
+  function load(filename) {
+    return JSON.parse(localStorage[filename]);
   }
 
   return {
     createSync: create,
     create: function(filename, curve, legacy, cb) {
-      if(isFunction(legacy))
-        cb = legacy, legacy = null
-      if(isFunction(curve))
-        cb = curve, curve = null
-      cb(null, create(filename, curve, legacy))
+      if (isFunction(legacy)) (cb = legacy), (legacy = null);
+      if (isFunction(curve)) (cb = curve), (curve = null);
+      cb(null, create(filename, curve, legacy));
     },
     loadSync: load,
-    load: function (filename, cb) {
-      cb(null, load(filename))
+    load: function(filename, cb) {
+      cb(null, load(filename));
     }
-  }
-
-}
-
-
+  };
+};
